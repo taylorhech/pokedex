e67d5003fd170a3d6a3305b263be20d1ee40653c
taylorhech <114107307+taylorhech@users.noreply.github.com>
2024-01-17 18:46:40 -0800

    Created new variable in scripts.js
    Deleted function alert & created a new variable "pokemonList" as an array including names, height, and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'noodles';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Tyranitar', height: 1.62, type: ['Dark', 'Rock']},
+    {name: 'Mamoswine', height: 3.02, type: ['Ice', 'Ground']},
+    {name: 'Rayquaza', height: 5.28, type: ['Dragon', 'Flying']}
+    ];
