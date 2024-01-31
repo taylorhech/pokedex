c2f2bdd88c0ca836bf6f052e4585e638e0733914
taylorhech <114107307+taylorhech@users.noreply.github.com>
2024-01-30 18:43:37 -0800

    Added IIFE- still needs adjusting

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ let pokemonList = [
     {name: 'Rayquaza', height: 5.28, types: ['Dragon', 'Flying']}
     ];
 
+// forEach loop
 pokemonList.forEach(function(pokemon){
     document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
     if (pokemon.height >=3.5) {
@@ -11,3 +12,28 @@ pokemonList.forEach(function(pokemon){
     }
     document.write('<br>')
 })
+
+// IIFE
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Tyranitar', height: 1.62, types: ['Dark', 'Rock']},
+        {name: 'Mamoswine', height: 3.02, types: ['Ice', 'Ground']},
+        {name: 'Rayquaza', height: 5.28, types: ['Dragon', 'Flying']}
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.add({name: 'Ralts'});
+document.write(pokemonRepository.getAll());
